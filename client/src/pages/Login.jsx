46d0dd3391f1fc03c28764ffcc6d972af8c235ac
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-01-16 15:05:53 +0100

    Redirect to dashboard on login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import Nav from '../components/Nav'
 import { onLogin } from '../services/userServices'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
-
+  const navigate = useNavigate()
   return (
     <main className='auth'>
       <Nav />
-      <form onSubmit={(e) => onLogin(e, userName, password)} className='auth__form wrapper'>
+      <form onSubmit={(e) => onLogin(e, userName, password, navigate)} className='auth__form wrapper'>
         <label className='auth__label'>Username</label>
         <input className='auth__input' onChange={(e) => setUserName(e.target.value)} />
         <label className='auth__label'>Password</label>
