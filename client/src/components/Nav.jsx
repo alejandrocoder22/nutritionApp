912748f50cba68162f919f7a1a34ff0e51d18860
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-01-16 15:05:23 +0100

    Added Logged in nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,39 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { UserContext } from '../context/userContext'
+import { logOut } from '../services/logOut'
 
 const Nav = () => {
+  const context = useContext(UserContext)
+  const navigate = useNavigate()
   return (
     <header className='header wrapper '>
-      <div className='header__logo'>Logo</div>
+      <Link to='/'>
+        <div className='header__logo'>Logo</div>
+      </Link>
       <nav className='header__nav'>
-        <ul className='nav__ul'>
-          <li className='nav__li'>Login</li>
-          <li className='nav__li'>Register</li>
-        </ul>
+        {
+          context.UserState?.isLogged
+            ? (
+              <ul className='nav__ul'>
+                <Link to='/login'>
+                  <li className='nav__li'>Login</li>
+                </Link>
+                <Link to='/register'>
+                  <li className='nav__li'>Register</li>
+                </Link>
+              </ul>
+              )
+            : (
+              <ul className='nav__ul'>
+                <Link to='/dashboard'>
+                  <li className='nav__li'>Dashboard</li>
+                </Link>
+                <li className='nav__li' onClick={() => logOut(navigate)}>Logout</li>
+              </ul>
+              )
+        }
+
       </nav>
     </header>
   )
