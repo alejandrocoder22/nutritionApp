94589bca1e2c5e21fe6a1c03112486a9437c5fac
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-01-16 15:05:42 +0100

    Added Link to home button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Nav from '../components/Nav'
 import lineSvg from '../images/line-home.svg'
-
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <main className='home'>
@@ -8,7 +8,9 @@ const Home = () => {
       <div className='home__intro-text wrapper'>
         <h1 className='intro-text__h1'>Don't <span className='intro-text__red-bg'> lose weight </span><br /> Gain habits</h1>
         <img className='intro-text__line' src={lineSvg} />
-        <button className='home__button radius pointer'>Start Here</button>
+        <Link to='/dashboard'>
+          <button className='home__button radius pointer'>Start Here</button>
+        </Link>
       </div>
     </main>
   )
