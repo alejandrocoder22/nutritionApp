e7470f69e1afc71c688c50aff0a4f1ee31b66b14
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-02-10 14:36:04 +0100

    Added parseDate function

---
+++ client/src/utils/parseDate.js
@@ -0,0 +1,24 @@
+export const parseDate = (dateToParse) => {
+  const getMonthNumber = {
+    Jan: '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12'
+  }
+
+  const dateSplited = dateToParse.split(' ')
+
+  const month = getMonthNumber[dateSplited[1]]
+  const year = dateSplited[3]
+  const day = dateSplited[2]
+
+  return `${year}-${month}-${day}`
+}
