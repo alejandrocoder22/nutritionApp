6e4fafe88de1b22e8a5624752774a372e0d69896
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-14 10:03:36 +0100

    Added styles to pieChart

--- client/src/components/userDashboard/PieChartDashboard.jsx
+++ client/src/components/userDashboard/PieChartDashboard.jsx
@@ -1,27 +1,47 @@
 
-import { PieChart, Pie } from 'recharts'
+import { PieChart, Pie, Cell, Legend } from 'recharts'
 import { sumAllMacroData } from '../../utils/sumAllMacroData'
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+
+const RADIAN = Math.PI / 180
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+  const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+  return (
+    <text x={x} y={y} fill='white' textAnchor={x > cx ? 'start' : 'end'} dominantBaseline='central'>
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  )
+}
 const PieChartDashboard = ({ foodDairy }) => {
   const data = [
     { name: 'Proteinas', value: sumAllMacroData(foodDairy, 'proteins') },
     { name: 'Fats', value: sumAllMacroData(foodDairy, 'fats') },
     { name: 'Carbs', value: sumAllMacroData(foodDairy, 'carbs') }
   ]
+
   return (
     <PieChart width={400} height={400}>
       <Pie
-        dataKey='value'
-        startAngle={180}
-        endAngle={0}
         data={data}
         cx='50%'
         cy='50%'
+        labelLine={false}
+        label={renderCustomizedLabel}
         outerRadius={80}
         fill='#8884d8'
-        label
-      />
+        dataKey='value'
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Legend />
     </PieChart>
   )
 }
+
 export default PieChartDashboard
--- client/src/components/userDashboard/UserDashboardGraph.jsx
+++ client/src/components/userDashboard/UserDashboardGraph.jsx
@@ -3,10 +3,9 @@ import PieChartDashboard from './PieChartDashboard'
 const UserDashboardGraph = ({ foodDairy }) => {
   return (
     <div className='graph'>
-      <div className='user-dashboard__info'>
-        Total
-      </div>
+
       <PieChartDashboard foodDairy={foodDairy} />
+
     </div>
   )
 }
