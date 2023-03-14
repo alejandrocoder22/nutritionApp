3e2da3bde7ec68a88c5fd3c8efeea225f68da763
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-14 10:49:32 +0100

    Added close popup on click

--- client/src/components/addToDairy/AddDairyPopup.jsx
+++ client/src/components/addToDairy/AddDairyPopup.jsx
@@ -1,8 +1,27 @@
 import { useSearchParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+
 const AddDairyPopup = ({ pickedFood, foodToAdd, setPickedFood, setPopup }) => {
   const [getParams] = useSearchParams()
   const [quantity, setQuantity] = useState(false)
+
+  function useOutsideAlerter (ref) {
+    useEffect(() => {
+      function handleClickOutside (event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setPopup(false)
+        }
+      }
+
+      document.addEventListener('mousedown', handleClickOutside)
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
+    }, [ref])
+  }
+  const wrapperRef = useRef(null)
+
+  useOutsideAlerter(wrapperRef)
 
   const onAddFood = (e) => {
     e.preventDefault()
@@ -17,7 +36,7 @@ const AddDairyPopup = ({ pickedFood, foodToAdd, setPickedFood, setPopup }) => {
   }
 
   return (
-    <form onSubmit={(e) => onAddFood(e)} className='add-to-dairy__popup'>
+    <form onSubmit={(e) => onAddFood(e)} className='add-to-dairy__popup' ref={wrapperRef}>
       <label>Quantity</label>
       <input onChange={(e) => setQuantity(e.target.value)} type='text' />
       <button>Add To Dairy</button>
