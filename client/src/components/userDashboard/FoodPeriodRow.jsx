c44ebdf4248629715f50a1511b8a367eb725103e
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-14 12:27:51 +0100

    Dinamic calculate total macros

--- client/src/components/userDashboard/FoodPeriodRow.jsx
+++ client/src/components/userDashboard/FoodPeriodRow.jsx
@@ -24,13 +24,12 @@ const FoodPeriodRow = ({ food, setFoodDairy, foodDairy }) => {
 
   return (
     <tr className='table__tr' onAnimationEnd={() => removeFoodInFrontend(food.dairy_id)}>
-      <td className='table__td left'>{food.food_name}</td>
+      <td className='table__td left capitalize'>{food.food_name}</td>
       <td className='table__td'>{food.grams}</td>
       <td className='table__td'>{Number(food.kcal) * Number(food.grams / 100)}</td>
-      <td className='table__td'>{food.carbs}</td>
-      <td className='table__td'>{food.fats}</td>
-      <td className='table__td'>{food.proteins}</td>
-
+      <td className='table__td'>{food.carbs * food.grams / 100}</td>
+      <td className='table__td'>{food.fats * food.grams / 100}</td>
+      <td className='table__td'>{food.proteins * food.grams / 100}</td>
       <MdDeleteOutline className='table__delete-icon' onClick={(e) => deleteFoodDairy(e, food.dairy_id)} />
     </tr>
   )
