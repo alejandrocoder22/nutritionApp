7c3c08f6b89731683a4aa6902a6389afdbc02796
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-15 14:33:40 +0100

    Change chart style

--- client/src/components/userDashboard/PieChartDashboard.jsx
+++ client/src/components/userDashboard/PieChartDashboard.jsx
@@ -1,45 +1,34 @@
 
-import { PieChart, Pie, Cell, Legend } from 'recharts'
+import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts'
 import { sumAllMacroData } from '../../utils/sumAllMacroData'
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
-
-const RADIAN = Math.PI / 180
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.50
-  const x = cx + radius * Math.cos(-midAngle * RADIAN)
-  const y = cy + radius * Math.sin(-midAngle * RADIAN)
-
-  return (
-    <text x={x} y={y} fill='white' textAnchor={x > cx ? 'start' : 'end'} dominantBaseline='central'>
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  )
-}
 const PieChartDashboard = ({ foodDairy }) => {
   const data = [
     { name: 'Proteinas', value: sumAllMacroData(foodDairy, 'proteins') },
     { name: 'Fats', value: sumAllMacroData(foodDairy, 'fats') },
     { name: 'Carbs', value: sumAllMacroData(foodDairy, 'carbs') }
   ]
-
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28']
   return (
-    <PieChart width={300} height={250}>
+    <PieChart width={250} height={350}>
       <Pie
         data={data}
-        cx='50%'
-        cy='50%'
-        labelLine={false}
-        label={renderCustomizedLabel}
+        cx={120}
+        cy={200}
+        innerRadius={50}
         outerRadius={80}
         fill='#8884d8'
+        paddingAngle={5}
         dataKey='value'
+        label
       >
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
         ))}
       </Pie>
       <Legend />
+      <Tooltip />
+
     </PieChart>
   )
 }
