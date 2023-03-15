80bee01e28c5d8c1eee96459f3c7fd68ae728995
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-15 13:49:33 +0100

    Added register option

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import { onLogin } from '../services/userServices'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Nav from '../components/ui/Nav'
 
 export const Login = () => {
@@ -9,14 +9,17 @@ export const Login = () => {
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
   return (
-    <main className='auth'>
+    <main className='auth wera'>
       <Nav />
-      <form onSubmit={(e) => onLogin(e, userName, password, navigate)} className='auth__form wrapper'>
-        <label className='auth__label'>Username</label>
-        <input className='auth__input' onChange={(e) => setUserName(e.target.value)} />
-        <label className='auth__label'>Password</label>
-        <input className='auth__input' onChange={(e) => setPassword(e.target.value)} type='password' />
-        <button className='auth__button pointer'>Login</button>
+      <form onSubmit={(e) => onLogin(e, userName, password, navigate)} className='auth__form wrapper '>
+        <div className='auth__wrapper'>
+          <label className='auth__label'>Username</label>
+          <input className='auth__input' onChange={(e) => setUserName(e.target.value)} />
+          <label className='auth__label'>Password</label>
+          <input className='auth__input' onChange={(e) => setPassword(e.target.value)} type='password' />
+          <p className='auth__register-text'>Not having an account? <Link to='/register'>Register here</Link> </p>
+          <button className='auth__button pointer'>Login</button>
+        </div>
       </form>
     </main>
   )
