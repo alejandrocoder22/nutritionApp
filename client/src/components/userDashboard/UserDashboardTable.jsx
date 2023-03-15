c8ad9fb059b9f076c25e8a018fbbe8533c48b55e
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-15 13:49:11 +0100

    Removed unused import

--- client/src/components/userDashboard/UserDashboardTable.jsx
+++ client/src/components/userDashboard/UserDashboardTable.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import AddToDairyButton from './AddToDairyButton'
 import FoodPeriodRow from './FoodPeriodRow'
 import UserDashboardTableHeader from './UserDashboardTableHeader'
