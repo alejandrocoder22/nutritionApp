66ccbe36020603536e2479d5c2c132d68e28de17
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-19 13:12:00 +0100

    Added userState to login function

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-
+import { useState, useContext } from 'react'
 import { onLogin } from '../services/userServices'
 import { Link, useNavigate } from 'react-router-dom'
 import Nav from '../components/ui/Nav'
+import { UserContext } from '../context/userContext'
 
 export const Login = () => {
+  const context = useContext(UserContext)
+
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
   return (
     <main className='auth wera'>
       <Nav />
-      <form onSubmit={(e) => onLogin(e, userName, password, navigate)} className='auth__form wrapper '>
+      <form onSubmit={(e) => onLogin(e, userName, password, navigate, context)} className='auth__form wrapper '>
         <div className='auth__wrapper'>
           <label className='auth__label'>Username</label>
           <input className='auth__input' onChange={(e) => setUserName(e.target.value)} />
