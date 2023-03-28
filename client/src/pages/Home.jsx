1c8b5f3e15f098ba11c828f564b9ccf6e34408e6
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-03-28 15:55:20 +0200

    Fixed spaces

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
     <main className='home'>
       <Nav />
       <div className='home__intro-text wrapper'>
-        <h1 className='intro-text__h1'>No <span className='intro-text__red-bg'> Pierdas peso </span><br /> Gana hábitos</h1>
+        <h1 className='intro-text__h1'>No <span className='intro-text__red-bg'>Pierdas peso</span><br /> Gana hábitos</h1>
         <img className='intro-text__line' src={lineSvg} />
         <Link to='/dashboard'>
           <button className='home__button radius pointer'>Comienza aquí</button>
